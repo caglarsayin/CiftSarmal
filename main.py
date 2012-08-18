483ee3e2a7632151383f4140c06f26321be674b8
caglar sayin <caglar@hotmail.com.tr>
2012-08-18 19:06:25 +0300

    v0.2

--- main.py
+++ main.py
@@ -15,28 +15,81 @@
 # limitations under the License.
 #
 import webapp2
+import datetime
+import jinja2
+import os
+import urllib
+
+from google.appengine.ext import db
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.api import users
 
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+
+class Article(db.Model):
+	title = db.StringProperty(required=True,indexed=False)
+	text = db.TextProperty(required=True,indexed=False)
+	imgKey = blobstore.BlobReferenceProperty(required=True,indexed=False)
+	additionDate = db.DateProperty(required=True,indexed=True)
+	editor = db.UserProperty(required=True,indexed=True)
+	tags = db.StringListProperty(indexed=True)
+	categ = db.StringProperty(choices=['dbilimleri', 'teknoloji', 'psikoloji', 'ekonomi','saglik','ekoloji','tbilimleri'],required=True,indexed=True )
+	count = db.IntegerProperty(indexed=False)
+	orjinalLink = db.LinkProperty(indexed=False)
+	relatedLinks = db.ListProperty(db.Link,indexed=False)
+	videoLings = db.ListProperty(db.Link,indexed=False)
+		
+		
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
-		file="static/listing.html"
-		listing=open(file).read()
+		filed="static/listing.html"
+		listing=open(filed).read()
 		self.response.out.write(listing)
 
-class postit(webapp2.RequestHandler):
+class postIt(webapp2.RequestHandler):
 	def get(self):
 		user = users.get_current_user()
 		if user:
-			file="static/postit.html"
-			posting=open(file).read()
-			self.response.out.write(posting)
+			postpage = blobstore.create_upload_url('/posting')
+			template = jinja_environment.get_template('static/postit.html')
+			self.response.out.write(template.render({'postpage':postpage}))
 		else:
 			self.redirect(users.create_login_url(self.request.uri))
 			
+class posting(blobstore_handlers.BlobstoreUploadHandler):
+	def post(self):
+		postedimg = self.get_uploads('img')  # 'file' is file upload field in the form
+		postedaudio = self.get_uploads('audio')
+		imgBlob = postedimg[0]
+		audioBlob = postedaudio[0]
+		dataModel = Article(title = self.request.get('baslik'),
+						text = db.Text(self.request.get('editor')),
+						orjinalLink = db.Link(self.request.get('link')),
+						tags = [self.request.get('tags'),],
+						rlinks = self.request.get('rlinks'),
+						categ = self.request.get('cate'),
+						imgKey = imgBlob.key(),
+						additionDate = datetime.datetime.now().date(),
+						editor = users.get_current_user()
+						)
+		dataModel.put()
+		self.redirect('/')
+		
+class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
+	def get(self, resource):
+		resource = str(urllib.unquote(resource))
+ 		blob_info = blobstore.BlobInfo.get(resource)
+ 		self.send_blob(blob_info)		
+
 app = webapp2.WSGIApplication(
 	[
 	('/', MainHandler),
-	('/postit',postit)
+	('/posting', posting),
+	('/postit',postIt),
+	('/serve/([^/]+)?', ServeHandler)
 	],debug=True)
 
 def main():
