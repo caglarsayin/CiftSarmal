f4fbaab3f7853d8401ef40a1c3dd4ff9a47d9a20
caglarsayin <T46jfJED9k>
2012-08-24 19:27:34 +0200

    Summary page's temp. exception branches is served

--- main.py
+++ main.py
@@ -28,87 +28,90 @@ from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.api import users
 
 jinja_environment = jinja2.Environment(
-	loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 
 class Article(db.Model):
-	title = db.StringProperty(required=True,indexed=False)
-	text = db.TextProperty(required=True,indexed=False)
-	imgKey = blobstore.BlobReferenceProperty(required=True,indexed=False)
-	additionDate = db.DateProperty(required=True,indexed=True)
-	summary = db.TextProperty(required=True,indexed=False)
-	editor = db.UserProperty(required=True,indexed=True)
-	tags = db.StringListProperty(indexed=True)
-	categ = db.StringProperty(choices=['dbilimleri', 'teknoloji', 'psikoloji', 'ekonomi','saglik','ekoloji','tbilimleri'],required=True,indexed=True )
-	count = db.IntegerProperty(indexed=False)
-	orjinalLink = db.LinkProperty(indexed=False)
-	relatedLinks = db.ListProperty(db.Link,indexed=False)
-	videoLings = db.ListProperty(db.Link,indexed=False)
+    title = db.StringProperty(required=True,indexed=False)
+    text = db.TextProperty(required=True,indexed=False)
+    imgKey = blobstore.BlobReferenceProperty(required=True,indexed=False)
+    additionDate = db.DateProperty(required=True,indexed=True)
+    summary = db.TextProperty(required=True,indexed=False)
+    editor = db.UserProperty(required=True,indexed=True)
+    tags = db.StringListProperty(indexed=True)
+    categ = db.StringProperty(choices=['dbilimleri', 'teknoloji', 'psikoloji', 'ekonomi','saglik','ekoloji','tbilimleri'],required=True,indexed=True )
+    count = db.IntegerProperty(indexed=False)
+    orjinalLink = db.LinkProperty(indexed=False)
+    relatedLinks = db.ListProperty(db.Link,indexed=False)
+    videoLings = db.ListProperty(db.Link,indexed=False)
 
 
 class MainHandler(webapp2.RequestHandler):
-	def get(self):
-		filed="static/listing.html"
-		listing=open(filed).read()
-		self.response.out.write(listing)
+    def get(self):
+        filed="static/listing.html"
+        listing=open(filed).read()
+        self.response.out.write(listing)
 
 class jinja(webapp2.RequestHandler):
-	def get(self):
-		template = jinja_environment.get_template('static/listing_jinja.html')
-		articles = Article.all()
-		self.response.out.write(template.render({'articles': articles}))
+    def get(self):
+        template = jinja_environment.get_template('static/listing_jinja.html')
+        articles = Article.all()
+        self.response.out.write(template.render({'articles': articles}))
 
 class postIt(webapp2.RequestHandler):
-	def get(self):
-		user = users.get_current_user()
-		if user:
-			postpage = blobstore.create_upload_url('/posting')
-			template = jinja_environment.get_template('static/postit.html')
-			self.response.out.write(template.render({'postpage':postpage}))
-		else:
-			self.redirect(users.create_login_url(self.request.uri))
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            postpage = blobstore.create_upload_url('/posting')
+            template = jinja_environment.get_template('static/postit.html')
+            self.response.out.write(template.render({'postpage':postpage}))
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
 
 class posting(blobstore_handlers.BlobstoreUploadHandler):
-	def post(self):
-		postedimg = self.get_uploads('img')  # 'file' is file upload field in the form
-		postedaudio = self.get_uploads('audio')
-		imgBlob = postedimg[0]
-		audioBlob = postedaudio[0]
-		dataModel = Article(title = unicode(self.request.get('baslik')),
-						text = db.Text(self.request.get('editor')),
-						orjinalLink = db.Link(self.request.get('link')),
-						tags = [self.request.get('tags'),],
-						rlinks = self.request.get('rlinks'),
-						categ = self.request.get('cate'),
-						summary = self.request.get('summary'),
-						imgKey = imgBlob.key(),
-						additionDate = datetime.datetime.now().date(),
-						editor = users.get_current_user()
-						)
-		dataModel.put()
+    def post(self):
+        try:
+            postedimg = self.get_uploads('img')  # 'file' is file upload field in the form
+            imgBlob = postedimg[0]
+            if self.get_uploads("audio"):
+                postedaudio = self.get_uploads('audio')
+                audioBlob = postedaudio[0]
+            dataModel = Article(title = unicode(self.request.get('baslik')),
+                            text = db.Text(self.request.get('editor')),
+                            orjinalLink = db.Link(self.request.get('link')),
+                            tags = [self.request.get('tags'),],
+                            rlinks = self.request.get('rlinks'),
+                            categ = self.request.get('cate'),
+                            summary = self.request.get('summary'),
+                            imgKey = imgBlob.key(),
+                            additionDate = datetime.datetime.now().date(),
+                            editor = users.get_current_user())
+            dataModel.put()
+        except:
+            self.redirect("/postit")
 
 class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
-	def get(self, resource):
-		"""
-		Blob store middle_ware
-		"""
-		resource = str(urllib.unquote(resource))
-		blob_info = blobstore.BlobInfo.get(resource)
-		self.send_blob(blob_info)
+    def get(self, resource):
+        """
+        Blob store middle_ware
+        """
+        resource = str(urllib.unquote(resource))
+        blob_info = blobstore.BlobInfo.get(resource)
+        self.send_blob(blob_info)
 
 
 app = webapp2.WSGIApplication(
-	[
-	('/orjinal', MainHandler),
-	('/', jinja),
-	('/posting', posting),
-	('/postit',postIt),
-	('/serve/([^/]+)?', ServeHandler)
-	],debug=True)
+    [
+    ('/orjinal', MainHandler),
+    ('/', jinja),
+    ('/posting', posting),
+    ('/postit',postIt),
+    ('/serve/([^/]+)?', ServeHandler)
+    ],debug=True)
 
 def main():
-	app.run(app)
+    app.run(app)
 
 
 if __name__ == '__main__':
-	main()
+    main()
